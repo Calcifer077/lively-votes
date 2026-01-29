5db41c52f5b417e061dd1a1801a0ce458bae911c
Calcifer077 <maheshnashier14@gmail.com>
2026-01-29 18:36:53 +0530

    Used useGetPollsWithPagination hook in UI

--- Frontend/src/components/AllPolls.jsx
+++ Frontend/src/components/AllPolls.jsx
@@ -2,29 +2,31 @@ import { css } from "@emotion/css";
 import Grid from "@mui/material/Grid";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
 
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 
 import Poll from "./Poll";
-import { getAllPolls } from "../services/apiPolls";
+// import { getAllPolls } from "../services/apiPolls";
 import { useAuthContext } from "../context/AuthContext";
+import { useGetPollsWithPagination } from "../features/polls/useGetPollsWithPagination";
 
 function AllPolls() {
+  // getting the currently logged in user
   const { userId } = useAuthContext();
 
-  const {
-    isPending,
-    isError,
-    data: pollsFromDB,
-  } = useQuery({
-    queryKey: ["polls"],
-    queryFn: getAllPolls,
-  });
+  // will get inifinte data in paginated form
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useGetPollsWithPagination();
 
-  if (isPending) {
+  function handleClick() {
+    fetchNextPage();
+  }
+
+  if (status === "loading") {
     return (
       <Box
-        key={"isPending"}
+        key={"isFetching"}
         sx={{
           display: "flex",
           justifyContent: "center",
@@ -37,7 +39,7 @@ function AllPolls() {
     );
   }
 
-  if (isError) {
+  if (status === "error") {
     return (
       <Box
         key={"error"}
@@ -64,24 +66,68 @@ function AllPolls() {
       className={css`
         margin-left: 20px;
         margin-top: 10px;
+        margin-bottom: 50px;
         z-index: 10;
       `}
     >
       <h1>All Polls</h1>
       <p>Cast your vote and see real-time results</p>
       <Grid container columns={16} spacing={4}>
-        {pollsFromDB?.data?.map((el) => (
-          <Grid size={5}>
-            <Poll
-              question={el.question}
-              options={el.options}
-              key={el.id}
-              byMe={userId && el.user_id === userId}
-              pollId={el.id}
-            ></Poll>
-          </Grid>
-        ))}
+        {/* Explanation of below two lines:
+        How does useInfinitQuery retrun data:
+        it returns something like this:
+        data = {
+          pages: [
+            { data: [poll1, poll2, poll3] }, // page 1
+            { data: [poll4, poll5, poll6] }, // page 2
+            { data: [poll7, poll8] },        // page 3
+          ],
+          pageParams: [1, 2, 3]
+        }
+        each time a next page is fetched it creates a new data object inside pages, so we flat it out.
+         */}
+
+        {data?.pages.flatMap((page) =>
+          page?.data.map((el) => (
+            <Grid size={5}>
+              <Poll
+                question={el.question}
+                options={el.options}
+                key={el.id}
+                byMe={userId && el.user_id === userId}
+                pollId={el.id}
+              ></Poll>
+            </Grid>
+          )),
+        )}
       </Grid>
+
+      {hasNextPage && (
+        <Button
+          variant="outlined"
+          disabled={isFetchingNextPage}
+          onClick={handleClick}
+          sx={{
+            display: "block",
+            margin: "auto",
+            border: "1.5px solid var(--text-gray)",
+            marginTop: "20px",
+            paddingLeft: "30px",
+            paddingRight: "30px",
+            color: "var(--dark-gray)",
+            "&:hover": {
+              backgroundColor: "var(--hover-indigo)",
+              color: "var(--light-gray)",
+            },
+            "&:active": {
+              backgroundColor: "var(--active-indigo)",
+              color: "var(--light-gray)",
+            },
+          }}
+        >
+          {isFetchingNextPage ? "Loading…" : "Load more"}
+        </Button>
+      )}
     </div>
   );
 }
