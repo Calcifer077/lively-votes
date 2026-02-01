f78c5e5fd065206222165b0219089f7c35127e4e
Calcifer077 <maheshnashier14@gmail.com>
2026-02-01 15:48:47 +0530

    Fixed error of CORS

--- Backend/app.js
+++ Backend/app.js
@@ -20,6 +20,7 @@ export const io = new Server(httpServer, {
   cors: {
     origin: allowedOrigins,
     methods: ["GET", "POST", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   },
 });
@@ -44,6 +45,7 @@ app.use(
   cors({
     origin: allowedOrigins,
     methods: ["GET", "POST", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
